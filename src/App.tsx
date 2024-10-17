0dcd55999f3acd57842acb086c34256105601fe7
moriroKim <moriroKim@gmail.com>
2024-10-17 17:11:39 +0900

    Chore: 브랜치 권한 테스트용 코드

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import useDarkModeStore from './stores/darkModeStore'
 import theme from './styles/theme'
 import CommonLayout from './components/Layout/CommonLayout'
 import ResetCSS from './styles/ResetCSS'
+import { useState } from 'react'
 
 function App() {
   const { isDarkMode } = useDarkModeStore()
+  const [state, setState] = useState(null)
   return (
     <>
       <ThemeProvider theme={isDarkMode ? theme.dark : theme.light}>
