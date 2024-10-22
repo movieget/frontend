f103ba1133e60c0da4d6030751b4d76f651022e2
moriroKim <moriroKim@gmail.com>
2024-10-23 00:50:20 +0900

    Chore: Booking Api 목업 데이터 & api  추가

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -5,6 +5,7 @@ import review_page3 from './review_page3.json'
 import review_page4 from './review_page4.json'
 import review_page5 from './review_page5.json'
 import review_page6 from './review_page6.json'
+import booking_info from './booking_info.json'
 
 const REVIEWS_PER_PAGE = 10
 
@@ -32,5 +33,29 @@ export const handlers = [
       currentPage: page,
       totalPages: Math.ceil(allReviews.length / REVIEWS_PER_PAGE),
     })
+  }),
+
+  http.get('/api/v1/book/screens', ({ request }) => {
+    const url = new URL(request.url)
+    const date = url.searchParams.get('date')
+
+    // 날짜 매개변수가 없으면 400 오류 응답 반환
+    if (!date) {
+      return HttpResponse.json({ error: 'Date parameter is required' }, { status: 400 })
+    }
+
+    // 주어진 날짜에 해당하는 상영 정보 필터링
+    const filteredData = booking_info.find((info) => info.date === date)
+
+    // 해당 날짜의 데이터가 없으면 404 오류 응답 반환
+    if (!filteredData) {
+      return HttpResponse.json(
+        { error: 'No screenings found for the provided date' },
+        { status: 404 },
+      )
+    }
+
+    // 해당 날짜의 데이터를 그대로 반환
+    return HttpResponse.json(filteredData)
   }),
 ]
