3356d15db0d6d6622913de160bb9b942bc7ec54b
moriroKim <moriroKim@gmail.com>
2024-10-23 00:56:26 +0900

    Feat: 예매정보 받아오는 BookingApi 추가

---
+++ src/apis/bookingApi.ts
@@ -0,0 +1,12 @@
+import { client } from './instances'
+
+export const fetchMovieData = async (date: string) => {
+  const formattedDate = date.slice(0, 10)
+  try {
+    const res = await client.get(`/api/v1/book/screens?date=${formattedDate}`)
+    const data = await res.data
+    console.log(data)
+  } catch (err) {
+    console.error(err)
+  }
+}
