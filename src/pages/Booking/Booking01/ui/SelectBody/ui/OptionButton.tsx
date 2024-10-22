01cf589c4bf0acccbaa7f77e96173325bdd0729f
moriroKim <moriroKim@gmail.com>
2024-10-22 18:11:45 +0900

    Refactor: OptionButton 컴포넌트를 타이틀에 따라 조건부로 상태변경하도록 수정

--- src/pages/Booking/Booking01/ui/SelectBody/ui/OptionButton.tsx
+++ src/pages/Booking/Booking01/ui/SelectBody/ui/OptionButton.tsx
@@ -3,14 +3,29 @@ import { useBookingStore } from '../../../../../../stores/store'
 import { OptionButtonStyle, OptionLabel } from '../SelectBody.style'
 
 interface IOptionButtonProps {
+  title: '영화' | '지역' | '영화관'
   age?: 'all' | 12 | 15 | 18
   label?: string
 }
 
-const OptionButton = ({ age, label = '옵션라벨' }: IOptionButtonProps) => {
+const OptionButton = ({ title, age, label = '옵션라벨' }: IOptionButtonProps) => {
   const setField = useBookingStore((state) => state.setField)
   const handleClick = () => {
-    setField('movie', label)
+    switch (title) {
+      case '영화':
+        setField('movie', label)
+        break
+      case '지역':
+        setField('location', label)
+        break
+      case '영화관':
+        setField('theater', label)
+        break
+      default:
+        throw new Error(
+          '타이틀값이 설정되어있지 않습니다! 영화 | 지역 | 영화관 중에 값을 설정해주세요',
+        )
+    }
   }
 
   return (
