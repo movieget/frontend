da2a20da1264c625fd198111ea2400a2da8f720d
moriroKim <moriroKim@gmail.com>
2024-10-22 18:17:08 +0900

    TimeButton 컴포넌트의 시간선택=> 전역상태관리 시작

--- src/pages/Booking/Booking01/ui/SelectBody/ui/TimeButton.tsx
+++ src/pages/Booking/Booking01/ui/SelectBody/ui/TimeButton.tsx
@@ -1,11 +1,20 @@
 import { BasicBtn } from '../../../../../../components/Button/style'
+import { useBookingStore } from '../../../../../../stores/store'
 
 interface ITimeButtonProps {
   time?: string
 }
 
 const TimeButton = ({ time = '00:00' }: ITimeButtonProps) => {
-  return <BasicBtn $size='medium'>{time}</BasicBtn>
+  const setField = useBookingStore((state) => state.setField)
+  const handleClick = () => {
+    setField('time', time)
+  }
+  return (
+    <BasicBtn $size='medium' onClick={handleClick}>
+      {time}
+    </BasicBtn>
+  )
 }
 
 export default TimeButton
