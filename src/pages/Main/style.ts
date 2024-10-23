44976704659865d4da406fedcbd15d2929f20b76
siltea <siltea2019@gmail.com>
2024-10-24 02:48:35 +0900

    WIP: 작업중간저장-트레일러부분 일부 수정 및 카드컴포넌트 테스트

--- src/pages/Main/style.ts
+++ src/pages/Main/style.ts
@@ -1,13 +1,14 @@
 import styled from 'styled-components'
 
-export const TrailerSection = styled.section`
+// 트레일러 구역
+export const StyleTrailerSection = styled.section`
   // 트레일러 전체구역
   height: 78rem;
   position: relative;
   background-color: black;
 `
 
-export const TrailerContents = styled.div`
+export const StyleTrailerContents = styled.div`
   // 트레일러 컨텐츠 표시구역 설정
   // 너비와 높이를 지정하지 않으면 컨텐츠 크기만큼 조정됨
   width: 128rem;
@@ -18,8 +19,30 @@ export const TrailerContents = styled.div`
   background-color: rebeccapurple;
 `
 
-export const TrailerWrapper = styled.div`
+export const StyleTrailerWrapper = styled.div`
   // 트레일러 영상 + 텍스트 묶는 구역
+
+  &::before {
+    content: '';
+    width: 100%;
+    height: 100%;
+    // 부모요소 기준
+    position: absolute;
+    left: 0;
+    top: 0;
+    // 외곽 연하게 조정 필요
+    background-image: linear-gradient(
+      to right,
+      #000 0%,
+      rgba(0, 0, 0, 0.25) 25%,
+      rgba(0, 0, 0, 0) 50%,
+      rgba(0, 0, 0, 0.25) 75%,
+      #000 100%
+    );
+    z-index: 2;
+  }
+`
+export const StyleTrailerText = styled.div`
   h1 {
     position: absolute;
     top: 16rem;
@@ -48,34 +71,52 @@ export const TrailerWrapper = styled.div`
     -webkit-box-orient: vertical;
     pointer-events: none;
   }
-
-  &::before {
-    content: '';
-    width: 100%;
-    height: 100%;
-    // 부모요소 기준
-    position: absolute;
-    left: 0;
-    top: 0;
-    // 외곽 연하게 조정 필요
-    background-image: linear-gradient(
-      to right,
-      #000 0%,
-      rgba(0, 0, 0, 0.25) 25%,
-      rgba(0, 0, 0, 0) 50%,
-      rgba(0, 0, 0, 0.25) 75%,
-      #000 100%
-    );
-    z-index: 2;
-  }
 `
-
-// 1.
-// 메인슬라이드 배너 -> 유튜브 트레일러 영상 => iframe
-
-// 슬라이드 배너 내부 : 영화제목/줄거리
-
 // 2. (1뒤에 2가 따라 붙어야 함)
 // 상영중 top10/개봉예정 top10 슬라이드형태
 // 영화리스트 포스터 내부 : 공통-연령/영화제목/좋아요/영화정보 버튼
-// 상영중 - 평점/예매하기
+// 상영중 - 평점/예매하기 버튼추가
+
+// 상영중
+
+// 상영예정
+
+// 영화 카드 컴포넌트
+
+export const StyleMovieCardWrapper = styled.div<{ isPlaying: boolean }>`
+  display: flex;
+
+  width: 100%;
+  height: 33.4rem;
+  position: relative;
+  background: #333;
+  color: #fff;
+  border-radius: 10px;
+  overflow: hidden;
+`
+
+export const StyleMovieCard = styled.div`
+  width: 22.4rem;
+  height: 33.4rem;
+  background-color: palevioletred;
+`
+
+export const StyleMovieTitle = styled.h3`
+  font-size: 1.5rem;
+  margin-bottom: 10px;
+`
+
+export const StyleLikeCount = styled.div`
+  margin-top: 10px;
+  font-size: 0.9rem;
+`
+
+export const StyleButton = styled.button`
+  margin-top: 15px;
+  padding: 10px 15px;
+  background-color: #6200ea;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`
