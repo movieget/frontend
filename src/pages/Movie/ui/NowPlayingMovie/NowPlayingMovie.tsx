224ea02c03f3fb94be7b721f7e535d76e1e1947e
kimdaesik <ds500033@naver.com>
2024-10-25 14:05:33 +0900

    Refactor: 목업 데이터 수정 기반으로 상영중/개봉예정 페이지 코드 수정

--- src/pages/Movie/ui/NowPlayingMovie/NowPlayingMovie.tsx
+++ src/pages/Movie/ui/NowPlayingMovie/NowPlayingMovie.tsx
@@ -7,7 +7,6 @@ interface NowPlayingMovieProps {
 }
 
 const NowPlayingMovie = ({ movieData }: NowPlayingMovieProps) => {
-  console.log(movieData)
   return (
     <MovieCardWrapper>
       {movieData?.map((movie) => (
