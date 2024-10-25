c8b4595b2a63fe41e4b99ab80001e05faae29be8
siltea <siltea2019@gmail.com>
2024-10-25 15:04:42 +0900

    Fix: MainList컴포넌트 타입수정

--- src/pages/Main/type.ts
+++ src/pages/Main/type.ts
@@ -6,3 +6,17 @@ export interface IfMovieCardProps {
   $playing?: 'playing' | 'notPlaying' | 'none'
   $isPlaying?: boolean
 }
+
+interface Movie {
+  id: number
+  movieId: number
+  posterImage: string
+  title: string
+  age?: 'all' | 12 | 15 | 18
+  totalLikes: number
+}
+
+export interface IfMovieListProps {
+  isPlayingMovies: Movie[]
+  notPlayingMovies: Movie[]
+}
