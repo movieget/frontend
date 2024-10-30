0a141df8b61732bbb4a07613323c78172cdb0c94
siltea <siltea2019@gmail.com>
2024-10-30 15:27:24 +0900

    feat: 로그인 서버통신부분 코드 일부 수정

--- src/pages/KakaoCallback/model.ts
+++ src/pages/KakaoCallback/model.ts
@@ -27,27 +27,47 @@ import { useUserStore } from '../../stores/userStore'
 // }
 
 // 백엔드에서 토큰 및 유저 정보 받아오는 함수
-// export const getAccessToken = async (code: string) => {
-//   try {
-//     // 인수로 auth code를 params로 전달
-//     const response = await client.get(`/api/v1/user/login/kakao?code=${code}`, {
-//       withCredentials: true,
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//     })
-//     // response.data에 객체의 형태로 값 저장 되어 반환
-//     // 전역 상태값에 넣지 않는 이유 : 이 함수는 API 호출 함수이기 때문에
-//     // 호출에 실패할경우 상태에 값을 전달하지 못하므로 error
-//     // const { access_token, user_id, profile_image, refresh_token } = response.data
-
-//     console.log('response data:', response.data)
-//     return response.data
-//   } catch (error) {
-//     console.error('Failed to login:', error)
-//     throw new Error('로그인 요청에 실패했습니다.')
-//   }
-// }
+export const getAccessToken = async () => {
+  try {
+    // 인수로 auth code를 params로 전달
+    const response = await client.get(`/api/v1/user/login/kakao`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    // response.data에 객체의 형태로 값 저장 되어 반환
+    // 전역 상태값에 넣지 않는 이유 : 이 함수는 API 호출 함수이기 때문에
+    // 호출에 실패할경우 상태에 값을 전달하지 못하므로 error
+    // const { access_token, user_id, profile_image, refresh_token } = response.data
+    console.log('response data:', response.data)
+    return response.data
+  } catch (err: any) {
+    // 상태 코드에 따른 분기 처리
+    if (err.response) {
+      const { status } = err.response
+      switch (status) {
+        case 400:
+          throw new Error('잘못된 요청입니다. 요청 내용을 확인하세요.')
+        case 401:
+          throw new Error('로그인 후 이용가능해요.')
+        case 403:
+          throw new Error('로그인 후 이용가능해요.')
+        case 404:
+          throw new Error('일치하는 유저정보가 없습니다.')
+        case 500:
+          throw new Error('서버 오류가 발생했습니다. 잠시 후 다시 시도하세요.')
+        default:
+          throw new Error(err.response.data?.error || '알 수 없는 오류가 발생했습니다.')
+      }
+    } else if (err.request) {
+      // 요청이 이루어졌지만 응답을 받지 못한 경우
+      throw new Error('서버와 연결할 수 없습니다. 인터넷 연결을 확인하세요.')
+    } else {
+      // 오류를 발생시킨 요청 설정 중 발생한 오류
+      throw new Error('요청 중 오류가 발생했습니다.')
+    }
+  }
+}
 
 // 유저인증및 유저정보 요청
 // export const getUser = async (token: string) => {
